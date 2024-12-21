7afcbf78305c6b464125bb6b20291b30172210bc
Smilez <smilezrealscripts@gmail.com>
2024-12-21 08:42:26 -0500

    Create Kade hub.lua

---
+++ scripts/Kade hub.lua
@@ -0,0 +1,6 @@
+if getgenv().KadeHubLoaded ~= true then
+    getgenv().KadeHubLoaded = true
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletfan2007/Work/main/latest.lua"))()
+else
+    game.StarterGui:SetCore("SendNotification",  { Title = "KadeHub"; Text = "KadeHub is already executed!"; Icon = "rbxassetid://17893547380"; Duration = 15; })
+end
