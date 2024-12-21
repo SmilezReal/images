1ef9f0fcab615b5370943be2edd5f188afb2594f
Smilez <smilezrealscripts@gmail.com>
2024-12-21 09:39:17 -0500

    Create Smilez Scriptz Universal | V3.0.lua

---
+++ scripts/Smilez Scriptz Universal | V3.0.lua
@@ -0,0 +1,22 @@
+Smilez Scriptz Universal | V3.0 Is A Roblox Script Slowly But Surely Adding Tons And Tons Of Popular Games From Roblox To Their Supported Games!
+
+This Script Supports Every Executor Including Solara
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmilezReal/SmilezScriptzUniversal/refs/heads/main/Loader.lua",true))()
+
+
+**Supported Games:**
+
+Prison Life
+
+Mega Fun Obby
+
+Hide And Seek Extreme
+
+Faction Defense Tycoon
+
+Murder Mystery 2
+
+Tower Of Hell
+
+Iron Man Simulator
