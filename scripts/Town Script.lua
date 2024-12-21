a6082cbca87cb927e79c1b9f1eabfb7437cfb6c5
Smilez <smilezrealscripts@gmail.com>
2024-12-21 08:44:03 -0500

    Create Town Script.lua

---
+++ scripts/Town Script.lua
@@ -0,0 +1,5 @@
+---- Key Brought To You By Get-Exploits! ----
+
+---Key: mightkms ----
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/blix200/FREE-TB2-SCRIPT/main/i-love-you-tecca"))()
