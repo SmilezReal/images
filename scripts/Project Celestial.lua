a3fe2d8179448b34eb6d98995b021d2d90ed83a6
Smilez <smilezrealscripts@gmail.com>
2024-12-21 08:44:20 -0500

    Create Project Celestial.lua

---
+++ scripts/Project Celestial.lua
@@ -0,0 +1,4 @@
+---- Go Here To Get The Key: https://celestialscript.store/GetKey/ ----
+
+getgenv().PublicKey = "Default" -- Replace "Default" with your key if you own premium.
+loadstring(game:HttpGet("https://celestialscript.store/API/MainAuth.php"))()
