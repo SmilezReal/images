f4ec8584c9c8d01fcb663b4bcd303b6adae9c532
Smilez/Get-Exploits <smilezrealscripts@gmail.com>
2025-01-13 11:23:36 -0500

    Update Kade hub.lua

--- scripts/Kade hub.lua
+++ scripts/Kade hub.lua
@@ -1,3 +1,4 @@
+---- Quick Access Brought To You By Get-Exploits ----
 if getgenv().KadeHubLoaded ~= true then
     getgenv().KadeHubLoaded = true
    loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletfan2007/Work/main/latest.lua"))()
