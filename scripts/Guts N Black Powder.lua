0dbf973e53b2c1916a56a1d86f63108059214361
Smilez/Get-Exploits <smilezrealscripts@gmail.com>
2025-01-13 11:22:00 -0500

    Update Guts N Black Powder.lua

--- scripts/Guts N Black Powder.lua
+++ scripts/Guts N Black Powder.lua
@@ -1,2 +1,3 @@
+---- Quick Access Brought To You By Get-Exploits ----
 local __function = loadstring(game:HttpGet("https://github.com/Hosvile/InfiniX/releases/latest/download/main.lua", true))
 __function(__function)
