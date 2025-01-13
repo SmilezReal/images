5d723ba209dee61895de8bcf3643571ebb83c125
Smilez/Get-Exploits <smilezrealscripts@gmail.com>
2025-01-13 11:21:47 -0500

    Update GAROU X KJ Moveset.lua

--- scripts/GAROU X KJ Moveset.lua
+++ scripts/GAROU X KJ Moveset.lua
@@ -1,1 +1,2 @@
+---- Quick Access Brought To You By Get-Exploits ----
 loadstring(game:HttpGet("https://raw.githubusercontent.com/damir512/garoukjv1maybeidk/main/Protected_2460290213750059.txt",true))()
